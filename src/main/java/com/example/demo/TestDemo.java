af6c508ec9a605aac6ea35b54fdac2e3c20540b3
srinivaas <snarajava@gmail.com>
2022-07-05 20:49:17 +0530

    newclasshas added as part of the userstory

---
+++ src/main/java/com/example/demo/TestDemo.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class TestDemo {
+// archana has added ad part some user story 
+	public void m1() {
+
+	}
+
+	public void m2() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
